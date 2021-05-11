7a1952c69d7864526a5308e64e3df7c87e74c0fb
shriram <shriramvivek08@gmail.com>
2021-05-11 18:47:07 +0530

    first commit

---
+++ prog4.py
@@ -0,0 +1,7 @@
+super_villains = {'Fiddler' : 'Isaac Bowin',
+                  'Captain Cold' : 'Leonard Snart',
+                  'Weather Wizard' : 'Mark Mardon',
+                  'Mirror Master' : 'Sam Scudder',
+                  'Pied Piper' : 'Thomas Peterson'}
+print(super_villains['Captain Cold'])
+ 
